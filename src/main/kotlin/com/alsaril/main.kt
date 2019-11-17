e0ce534d03d09945baa500516312862338436fc1
igor.yakovlev <igor.yakovlev@corp.mail.ru>
2019-11-17 14:23:23 +0300

    realtime factor change

--- src/main/kotlin/com/alsaril/main.kt
+++ src/main/kotlin/com/alsaril/main.kt
@@ -5,12 +5,23 @@ import java.awt.event.KeyEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
 
-data class Line(val x1: Double, val y1: Double, val x2: Double, val y2: Double)
+data class Line(
+    var x1: Double,
+    var y1: Double,
+    var x2: Double,
+    var y2: Double,
+    val parent: Line?,
+    val index: Int,
+    var version: Int
+)
 
 fun main() {
     val lines = Collections.synchronizedList(LinkedList<Line>())
-    lines.add(Line(-50.0, -50.0, 50.0, -50.0))
+    val version = AtomicInteger()
+    val root = Line(-50.0, -50.0, 50.0, -50.0, null, 0, 0)
+    lines.add(root)
 
     val w = 800
     val h = 800
@@ -19,6 +30,9 @@ fun main() {
     screen.setDrawListener { p ->
         p.clear()
         lines.forEach { l ->
+            if (l.version != version.get()) {
+                relocate(l, version.get())
+            }
             repeat(4) {
                 screen.reset()
                 screen.rotate(90 * it)
@@ -31,7 +45,7 @@ fun main() {
 
     screen.addMouseListener(object : MouseAdapter() {
         override fun mouseClicked(e: MouseEvent) {
-            update(lines)
+            update(lines, version.get())
             screen.redraw()
         }
     })
@@ -46,10 +60,18 @@ fun main() {
                 's' -> ty -= 20
                 'a' -> tx += 20
                 'd' -> tx -= 20
+                'z' -> {
+                    FACTOR += 0.1
+                    version.incrementAndGet()
+                }
+                'x' -> {
+                    FACTOR -= 0.1
+                    version.incrementAndGet()
+                }
             }
             val k = scale / oldScale
             tx = w / 2 * (1 - k) + k * tx // долбанутые формулы, на которые пара часов ушла, ахах
-            ty  = h / 2 * (1 - k) + k * ty
+            ty = h / 2 * (1 - k) + k * ty
             screen.redraw()
         }
     })
@@ -57,10 +79,66 @@ fun main() {
     screen.show()
 }
 
-fun update(lines: MutableList<Line>) {
+fun relocate(line: Line, version: Int) {
+    if (line.version == version || line.parent == null) return
+    relocate(line.parent, version)
+    val (xf, yf, xt, yt) = line.parent
+
+    val dx = (xt - xf) / 3
+    val dy = (yt - yf) / 3
+
+    val x1 = xf + dx
+    val y1 = yf + dy
+
+    val x2 = xt - dx
+    val y2 = yt - dy
+
+    val x3 = x1 + dy * FACTOR
+    val y3 = y1 - dx * FACTOR
+
+    val x4 = x2 + dy * FACTOR
+    val y4 = y2 - dx * FACTOR
+
+    when (line.index) {
+        0 -> {
+            line.x1 = xf
+            line.y1 = yf
+            line.x2 = x1
+            line.y2 = y1
+        }
+        1 -> {
+            line.x1 = x1
+            line.y1 = y1
+            line.x2 = x3
+            line.y2 = y3
+        }
+        2 -> {
+            line.x1 = x3
+            line.y1 = y3
+            line.x2 = x4
+            line.y2 = y4
+        }
+        3 -> {
+            line.x1 = x4
+            line.y1 = y4
+            line.x2 = x2
+            line.y2 = y2
+        }
+        4 -> {
+            line.x1 = x2
+            line.y1 = y2
+            line.x2 = xt
+            line.y2 = yt
+        }
+    }
+    line.version++
+}
+
+fun update(lines: MutableList<Line>, version: Int) {
     val iterator = lines.listIterator()
     while (iterator.hasNext()) {
-        val (xf, yf, xt, yt) = iterator.next()
+        val line = iterator.next()
+        val (xf, yf, xt, yt) = line
         iterator.remove()
 
         val dx = (xt - xf) / 3
@@ -78,16 +156,16 @@ fun update(lines: MutableList<Line>) {
         val x4 = x2 + dy * FACTOR
         val y4 = y2 - dx * FACTOR
 
-        iterator.add(Line(xf, yf, x1, y1))
-        iterator.add(Line(x1, y1, x3, y3))
-        iterator.add(Line(x3, y3, x4, y4))
-        iterator.add(Line(x4, y4, x2, y2))
-        iterator.add(Line(x2, y2, xt, yt))
+        iterator.add(Line(xf, yf, x1, y1, line, 0, version))
+        iterator.add(Line(x1, y1, x3, y3, line, 1, version))
+        iterator.add(Line(x3, y3, x4, y4, line, 2, version))
+        iterator.add(Line(x4, y4, x2, y2, line, 3, version))
+        iterator.add(Line(x2, y2, xt, yt, line, 4, version))
     }
 }
 
-val FACTOR = 5
-
+@Volatile
+var FACTOR = 5.0
 @Volatile
 var scale = 1.0
 @Volatile
